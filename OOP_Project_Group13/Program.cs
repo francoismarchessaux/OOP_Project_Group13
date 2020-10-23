ea556be7de5da30f8ee3c724061d5f740bc34fe1
francoismarchessaux <71280854+francoismarchessaux@users.noreply.github.com>
2020-10-23 20:29:58 +0100

    First Commit

--- OOP_Project_Group13/Program.cs
+++ OOP_Project_Group13/Program.cs
@@ -17,6 +17,7 @@ namespace OOP_Project_Group13
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.ReadLine();
         }
     }
 }
