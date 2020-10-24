1ef708dda5cc2ea829843fb1ac8ab2ae4bef3a28
Théotime FROGET <frogetheotime@gmail.com>
2020-10-24 18:09:35 +0100

    Create Class Subject

---
+++ OOP_Project_Group13/Subject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Project_Group13
+{
+    public class Subject
+    {
+        public string name { get; set; }
+        public Subject(string name)
+        {
+            this.name = name;
+        }
+    }
+}
