01967bd6161dc2a176f10813d130a21e427d7fe0
francoismarchessaux <71280854+francoismarchessaux@users.noreply.github.com>
2020-10-24 22:32:34 +0100

    Updated files

--- OOP_Project_Group13/Users/Administrator.cs
+++ OOP_Project_Group13/Users/Administrator.cs
@@ -14,18 +14,6 @@ namespace OOP_Project_Group13.Users
         
         }
 
-        public Course CreateCourse()
-        {
-            Course cours1 = new Course();
-            return cours1;
-        }
-
-        public Student CreateStudent(string _name, string _firstName, string _status, string _password, int _ID, string _mail, int _phone)
-        {
-            Student student1 = new Student();
-            return student1;
-        }
-
         public void RegisterStudent(Student student, Course course)
         {
 
