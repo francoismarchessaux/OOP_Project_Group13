edc601beacd06c7fd35e14cf0ba07207f753ce7e
francoismarchessaux <71280854+francoismarchessaux@users.noreply.github.com>
2020-10-24 16:16:01 +0100

    Modified Main Menu

--- OOP_Project_Group13/MainMenu.Designer.cs
+++ OOP_Project_Group13/MainMenu.Designer.cs
@@ -28,20 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(513, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1102, 646);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.button1);
+            this.Name = "MainMenu";
+            this.Text = "Main Menu";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
