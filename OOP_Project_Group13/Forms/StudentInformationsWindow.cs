ce58a7c56a0c05adc42345491ee65837cf45b0da
Théotime FROGET <frogetheotime@gmail.com>
2020-11-02 13:15:03 +0000

    Grades Table

--- OOP_Project_Group13/Forms/StudentInformationsWindow.cs
+++ OOP_Project_Group13/Forms/StudentInformationsWindow.cs
@@ -33,17 +33,30 @@ namespace OOP_Project_Group13
             MailLabel.Text = "Mail : " + student.mail;
             PhoneLabel.Text = "Phone number : " + student.phone;
             StudentPicture.ImageLocation = student.profilePicture;
-            AddCourse("Algo");
-            AddCourse("Maths");
+            List<PanelCourse> listCourses = new List<PanelCourse>();
+            PanelCourse algo = AddCourse("Algo",listCourses);
+            PanelCourse maths = AddCourse("Maths", listCourses);
+            PanelCourse physique = AddCourse("Maths", listCourses);
+            LabelGrade cc1 = AddGrade("CC1");
+            LabelGrade quizz1 = AddGrade("Quizz1");
+            PlaceGrade(algo, cc1, listCourses);
+            PlaceGrade(algo, quizz1,listCourses);
+            LabelGrade cc1Maths = AddGrade("CC1");
+            PlaceGrade(maths, cc1Maths, listCourses);
+
         }
 
         private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
-        private void AddCourse(string name)
+        private PanelCourse AddCourse(string name,List<PanelCourse> listCourses)
         {
-            Panel newPan = new Panel();
+            Label courseName = new Label();
+            Label averageLabel = new Label();
+            Label average = new Label();
+            List<LabelGrade> grades= new List<LabelGrade>();
+            PanelCourse newPan = new PanelCourse(courseName,averageLabel,average,grades);
             newPan.Height = 70;
             newPan.Width = 752;
             int x = 0;
@@ -57,25 +70,55 @@ namespace OOP_Project_Group13
             y += count * 80;
             generalPanel.Controls.Add(newPan);
             newPan.Location = new Point(x, y);
-            Label courseName = new Label();
             courseName.Text = name;
             courseName.AutoSize = true;
             courseName.Visible = true;
             newPan.Controls.Add(courseName);
             courseName.Location = new Point(10, 10);
-            Label averageLabel = new Label();
             averageLabel.Text = "Average : ";
             averageLabel.AutoSize = true;
             averageLabel.Visible = true;
             newPan.Controls.Add(averageLabel);
             averageLabel.Location = new Point(650,10);
-            Label average = new Label();
             average.Text = "00.00";
             average.AutoSize = true;
             average.Visible = true;
             newPan.Controls.Add(average);
             average.Location = new Point(710, 10);
-
+            listCourses.Add(newPan);
+            return newPan;
+        }
+        private LabelGrade AddGrade(string name)
+        {
+            Label gradeName = new Label();
+            gradeName.Text = name;
+            gradeName.AutoSize = true;
+            Label grade = new Label();
+            grade.Text = "00.00";
+            grade.AutoSize = true;
+            LabelGrade addGrade = new LabelGrade(gradeName,grade);
+            return addGrade;
+         }
+        private void PlaceGrade(PanelCourse course ,LabelGrade grade,List<PanelCourse> listCourses)
+        {
+            course.grades.Add(grade);
+            course.Controls.Add(grade.name);
+            course.Controls.Add(grade.grade);
+            int count = course.grades.Count;
+            int y = 25*count;
+            grade.name.Location = new Point(200, y);
+            grade.grade.Location = new Point(710, y);
+            grade.name.Visible = true;
+            grade.grade.Visible = true;
+            int index = 0;
+            while (course != listCourses[index])
+                index++;
+            listCourses[index].Height += 20;
+            for (int i = index+1; i < listCourses.Count; i++)
+            {
+                int pos = listCourses[i].Location.Y;
+                listCourses[i].Location = new Point(0, pos + 20);
+            }
         }
     }
 }
---
+++ OOP_Project_Group13/LabelGrade.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_Project_Group13
+{
+    class LabelGrade
+    {
+        public Label name { get; set; }
+        public Label grade { get; set; }
+        public LabelGrade(Label name,Label grade)
+        {
+            this.name = name;
+            this.grade = grade;
+        }
+
+    }
+}
