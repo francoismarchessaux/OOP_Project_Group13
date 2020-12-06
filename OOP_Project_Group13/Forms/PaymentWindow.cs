b9efdacb1b1cd944090bfca7b3cd2c9a488b91bb
VictorGuy94 <73124237+VictorGuy94@users.noreply.github.com>
2020-12-06 16:31:49 +0000

    Update PaymentWindow.cs

--- OOP_Project_Group13/Forms/PaymentWindow.cs
+++ OOP_Project_Group13/Forms/PaymentWindow.cs
@@ -28,7 +28,7 @@ namespace OOP_Project_Group13.Forms
         }
 
         /// <summary>
-        /// Loads the panel in which the name of the student is written and adapts the "dept" label depending on the student's fees
+        /// Loads the window with the panel in which the name of the student is written and adapts the "dept" label depending on the student's fees
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
